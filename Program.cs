90e5a2e00af15708c617c913a14b9ac0c41ff956
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-08-19 21:45:52 +0200

    refactor: edit Program.cs with Add Method LoadConfig and Add Method StartApp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using OmniSedeBackend.Config;
 using OmniSedeBackend.Models;
 using OmniSedeBackend.Repositories.Implementations;
 using OmniSedeBackend.Repositories.Interfaces;
@@ -6,20 +7,11 @@ using OmniSedeBackend.Repositories.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 string conn = LoadDatabase(builder.Configuration);
 
-LoadService(builder.Services, conn);
+LoadConfig(builder.Services, builder.Configuration, conn);
+LoadRepository(builder.Services);
+LoadService(builder.Services);
 
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.MapControllers();
-
-app.Run();
+StartApp(builder.Build());
 
 static string LoadDatabase(ConfigurationManager config)
 {
@@ -27,13 +19,16 @@ static string LoadDatabase(ConfigurationManager config)
     return Environment.ExpandEnvironmentVariables(connectionDbFind ?? "");
 }
 
-static void LoadService(IServiceCollection services, string conn)
+static void LoadConfig(IServiceCollection services, IConfiguration configuration, string conn)
 {
     services.AddDbContext<OmnisedeContext>(options => options.UseSqlServer(conn));
-    LoadRepository(services);
-    services.AddControllers();
-    services.AddEndpointsApiExplorer();
-    services.AddSwaggerGen();
+    services.Configure<JwtSettings>(configuration.GetSection("Jwt"));
+    services.PostConfigure<JwtSettings>(jwt =>
+    {
+        jwt.Issuer = Environment.ExpandEnvironmentVariables(jwt.Issuer ?? string.Empty);
+        jwt.Audience = Environment.ExpandEnvironmentVariables(jwt.Audience ?? string.Empty);
+        jwt.SecretKey = Environment.ExpandEnvironmentVariables(jwt.SecretKey ?? string.Empty);
+    });
 }
 
 static void LoadRepository(IServiceCollection services)
@@ -45,4 +40,25 @@ static void LoadRepository(IServiceCollection services)
     services.AddScoped<IRuoliRepository, RuoliRepository>();
     services.AddScoped<ISedeRepository, SedeRepository>();
     services.AddScoped<IUnitOfWork, UnitOfWork>();
+}
+
+static void LoadService(IServiceCollection services)
+{
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
+
+static void StartApp(WebApplication app)
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.Run();
 }
