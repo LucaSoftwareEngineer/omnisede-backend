84302b55877819d6111869febb2ca3a5ea9a3198
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-08-19 22:05:26 +0200

    refactor: replace Username with Email in LoginRequest DTO

--- Dto/Request/LoginRequest.cs
+++ Dto/Request/LoginRequest.cs
@@ -5,7 +5,7 @@ namespace OmniSedeBackend.Dto.Request;
 public class LoginRequest
 {
     [Required(ErrorMessage = "Email is required")]
-    public string Username { get; set; } = String.Empty;
+    public string Email { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = String.Empty;
